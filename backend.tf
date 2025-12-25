e31bdc165b522d0a5fc083caaea1645177d3d56b
raqimsaifi-boop <raqimsaifi@gmail.com>
2025-12-25 17:29:11 +0530

    .

--- backend.tf
+++ backend.tf
@@ -2,8 +2,9 @@ terraform {
   backend "s3" {
     bucket               = "tek-bv-terraform-state"
     region               = "ap-south-1"
-    dynamodb_table       = "terraform-locks"
+    key                  = "terraform.tfstate"        # REQUIRED: path in the bucket
     encrypt              = true
-    workspace_key_prefix = "ec2-provisioning"
+    workspace_key_prefix = "ec2-provisioning"         # optional, used with workspaces
+    use_lockfile         = true                       # S3-native locking (recommended)
   }
 }
